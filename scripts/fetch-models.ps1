583f5abfc8ce536184c99e297e18b3aac199a061
Kim Seong Yong <91203858+bequiett@users.noreply.github.com>
2026-09-03 03:14:55 +0900

    fetch dfn3 h0

--- scripts/fetch-models.ps1
+++ scripts/fetch-models.ps1
@@ -1,12 +1,16 @@
 $ErrorActionPreference = 'Stop'
-New-Item -ItemType Directory -Force assets | Out-Null
-
-Invoke-WebRequest `
-  'https://raw.githubusercontent.com/Xiaobin-Rong/gtcrn/main/stream/onnx_models/gtcrn_simple.onnx' `
-  -OutFile 'assets/gtcrn_simple.onnx'
-
-Invoke-WebRequest `
-  'https://raw.githubusercontent.com/snakers4/silero-vad/master/src/silero_vad/data/silero_vad.onnx' `
-  -OutFile 'assets/silero_vad.onnx'
-
-Write-Host "Models downloaded."
+$modelDir = 'assets/dfn3_h0'
+New-Item -ItemType Directory -Force $modelDir | Out-Null
+$base = 'https://raw.githubusercontent.com/shimondoodkin/deepfilter-rt/f1a1b27be767a5e62ccc8625017f1737c33a3bf9/models/dfn3_h0'
+$files = @(
+  'config.ini',
+  'combined_streaming.onnx',
+  'enc_conv_streaming.onnx',
+  'enc_gru_streaming.onnx',
+  'erb_dec_streaming.onnx',
+  'df_dec_streaming.onnx'
+)
+foreach ($file in $files) {
+  Invoke-WebRequest "$base/$file" -OutFile "$modelDir/$file"
+}
+Write-Host 'DFN3-H0 models downloaded.'
