954672c2f5aed72261f59a67b6d84229c3d37a28
Kim Seong Yong <91203858+bequiett@users.noreply.github.com>
2026-09-03 16:57:08 +0900

    switch model fetch to DPDFNet8

--- scripts/fetch-models.ps1
+++ scripts/fetch-models.ps1
@@ -1,16 +1,6 @@
 $ErrorActionPreference = 'Stop'
-$modelDir = 'assets/dfn3_h0'
+$modelDir = 'assets/dpdfnet8'
 New-Item -ItemType Directory -Force $modelDir | Out-Null
-$base = 'https://raw.githubusercontent.com/shimondoodkin/deepfilter-rt/f1a1b27be767a5e62ccc8625017f1737c33a3bf9/models/dfn3_h0'
-$files = @(
-  'config.ini',
-  'combined_streaming.onnx',
-  'enc_conv_streaming.onnx',
-  'enc_gru_streaming.onnx',
-  'erb_dec_streaming.onnx',
-  'df_dec_streaming.onnx'
-)
-foreach ($file in $files) {
-  Invoke-WebRequest "$base/$file" -OutFile "$modelDir/$file"
-}
-Write-Host 'DFN3-H0 models downloaded.'
+$model = 'https://huggingface.co/Ceva-IP/DPDFNet/resolve/main/onnx/dpdfnet8_48khz_hr.onnx?download=true'
+Invoke-WebRequest $model -OutFile "$modelDir/dpdfnet8_48khz_hr.onnx"
+Write-Host 'DPDFNet8 48 kHz HR model downloaded.'
